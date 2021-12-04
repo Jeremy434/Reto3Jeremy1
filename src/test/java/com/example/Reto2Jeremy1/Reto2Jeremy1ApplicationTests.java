4e7ecc868d29711ce540f90335b782102ea702ba
jeremy434 <jeremy.mejia.mt@correo.usa.edu.co>
2021-12-03 23:17:00 -0500

    Reto2Jeremy1

---
+++ src/test/java/com/example/Reto2Jeremy1/Reto2Jeremy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reto2Jeremy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto2Jeremy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
